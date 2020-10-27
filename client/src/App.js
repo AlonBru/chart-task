7d62a91ac5a47474cbb2aecc444c49e0d8d5116b
AlonBru <indyalon@gmail.com>
2020-10-28 00:14:08 +0200

    added 2nd chart
    world total confirmed

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import {useEffect, useState} from 'react'
-import {Brush,Line,LineChart,XAxis,YAxis,CartesianGrid,Legend,Tooltip} from 'recharts'
+import {Brush,Line,LineChart,XAxis,YAxis,CartesianGrid,Legend,Tooltip, Label} from 'recharts'
 import axios from 'axios'
 import GraphSelector from './GraphSelector'
 
 function App() {
   const [data,setData] =useState()
+  const [world,setWorld] =useState()
   const [selected,setSelected] = useState([
     {
       place:'Israel',
@@ -16,14 +17,16 @@ function App() {
       color:'#00aa00'
     }
   ])
-
+  const w =window.innerWidth-50
   const places  = data?Object.keys(data[3]).slice(1):null
-
+  console.log('pla',places)
   useEffect(()=>{
     axios.get('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv ')
     .then(({data:csv})=>{
-      const countries = csv.split('\n')
-      .map(dataBlock=>{
+      const dataBlocks = csv.split('\n')
+      dataBlocks.pop() // the last \n is in the array as an empty string
+      const countries = dataBlocks
+      .map((dataBlock,i)=>{
         const regex = /".*,.*"/
         //some data lines have names matching the regex (Namely Korea), this is to fix that.
         if(dataBlock.search(regex) !== -1){
@@ -36,8 +39,13 @@ function App() {
         }
       })
       const titles = countries.shift(0)
+      
       const dates = titles.slice(4).map((date,i)=>{
-        const dateObject = {date:date}
+        let fixedDate = date.split('/')
+        let month = fixedDate.shift()
+        fixedDate.splice(1,0,month)
+        const dateObject = {date:fixedDate.join('/')}
+        i===2&&console.log(1,dateObject)
         countries.forEach(country=>{
           if(dateObject[country[1]]){
             dateObject[country[1]] += Number(country[i+4])
@@ -45,17 +53,27 @@ function App() {
             dateObject[country[1]] = Number(country[i+4])
           }
         })
+        i===2&&console.log(2,dateObject)
         return dateObject
       })
-      console.log(dates)
       setData(dates)
+      const worldData = dates.map((dateData=>{
+        let {date} = dateData
+        delete dateData.date
+        const worldTotal = Object.keys(dateData)
+        .map(country=>dateData[country])
+        .reduce((acc,cur)=>acc+cur,0)
+        return {date, worldTotal }
+      }))
+      setWorld(worldData)
     })
   },[])
  
+  data&&console.log('data',data.map(d=>d.date))
+  console.log('world',world)
   const addLine = () => {
     let addSelection = [...selected]
     addSelection.push({place:'Israel',color:'#ff0000'})
-    console.log(addSelection)
     setSelected(addSelection)
   }
 
@@ -69,11 +87,12 @@ function App() {
     let newSelection = [...selected]
     newSelection[i] = newSetting
     setSelected(newSelection)
-
+    
   }
   return (
     <div className="App">
-    <button onClick={addLine} >+</button>
+    <h2>Total Confirmed Patients per Country</h2>
+    <button onClick={addLine} >add Country to graph</button>
       {data
       ?<>
       { 
@@ -86,28 +105,47 @@ function App() {
           current={selected[i]}
           />
         })
-      }
-      <LineChart width={700} height={700} data={data} 
+      } 
+      <LineChart width={w} height={700} data={data} 
       margin={{ top: 5, right: 30, left: 20, bottom: 5 }}> 
         <CartesianGrid strokeDasharray="3 3" />
         <XAxis dataKey="date" />
-        <YAxis type="number"/>
+        <YAxis type="number"  />
         <Brush />
         <Tooltip />
-        <Legend onClick={(e)=>console.log(e)} />
+        <Legend />
         {
          selected.map( ( { place, color } ) =>{
           return <Line 
           type="monotone" 
           dataKey={place} 
           stroke={color}
+          dot={false}
           key={place} />
 
          })
 
         }
       </LineChart>
-      </>
+      <h2>Total Confirmed Patient Worldwide</h2>     
+      <LineChart width={w} height={700} data={world} 
+      margin={{ top: 5, right: 30, left: 20, bottom: 5 }}> 
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis  dataKey="date" />
+        <YAxis type="number"/>
+        <Brush />
+        <Tooltip />
+        <Legend 
+          iconType='wye'
+        />
+        <Line 
+        type="monotone" 
+        dataKey='worldTotal'
+        stroke='#d00'
+        dot={false}
+         />
+      </LineChart>
+      </> 
       :null}
     </div>
   );
